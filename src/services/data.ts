183f7888d25ef522459bff994d61f82500f1102b
Khalid Zoabi <kzoabi@outlook.com>
2019-07-18 10:05:04 -0400

    Adding several fields to Endpoint, including...
    - name, for a normalized title, used as an id and in the url
    - feedback, an array of user-submitted messages
    - messages, an array of admin-provided messages
    
    Also added proper uri encoding and sample data.
    
    Also adding a yet unused reset method.

--- src/services/data.ts
+++ src/services/data.ts
@@ -5,6 +5,7 @@ export type Status = 'Operational' | 'Maintenance' | 'Down'
 
 export interface Endpoint {
   id: string
+  name: string
   title: string
   location: string
   status: Status
@@ -23,12 +24,17 @@ export interface Endpoint {
     lastCheck: string
     nextCheck: string
   }>
-}
-
-export interface Message {
-  id: string
-  content: string
-  status: Status
+  feedback: Array<{
+    submitted: string,
+    content: string
+  }>
+  messages: Array<{
+    submitted: string
+    content: string
+    summary: string
+    status: Status
+    active: boolean
+  }>
 }
 
 export default {
@@ -44,42 +50,63 @@ export default {
     return (endpoints.length > 0) ? endpoints : samples.map(sample => {
       return {
         id: uuid(),
+        name: encodeURIComponent(sample.title.toLowerCase()),
         title: sample.title,
-        location: 'https://example.com/backend',
+        location: `https://example.com/${encodeURIComponent(sample.title.toLowerCase())}`,
         status: sample.status,
-        requests: [
-          {
-            start: moment().subtract(1, 'days').toString(),
-            end: moment().subtract(1, 'days').toString(),
+        requests: [ 5, 4, 3, 2, 1 ].map(n => {
+          return {
+            start: moment().subtract(n, 'days').format('YYYY-MM-DD'),
+            end: moment().subtract(n, 'days').add(2, 'seconds').format('YYYY-MM-DD'),
             latency: '2000',
             response: {
-              type: 'success',
+              type: 'error',
               message: '200 OK',
               raw: ''
             }
           }
-        ],
+        }),
         checks: [
           {
             cron: '@hourly',
-            lastCheck: moment().subtract(1, 'days').toString(),
-            nextCheck: moment().toString()
+            lastCheck: moment().calendar(),
+            nextCheck: moment().add(1, 'hours').calendar()
           }
-        ]
+        ],
+        feedback: [
+          {
+            submitted: moment().subtract(3, 'months').subtract(4, 'hours').subtract(23, 'minutes').calendar(),
+            content: 'You guys are awesome! The service is so fast. Keep the good work going!'
+          },
+          {
+            submitted: moment().subtract(1, 'days').subtract(14, 'seconds').calendar(),
+            content: 'That has to be a fake review down there. This has been down for hours and no-one is on-call!'
+          }
+        ],
+        messages: (sample.title === 'API') ? [
+          {
+            submitted: moment().subtract(3, 'hours').calendar(),
+            content: ''
+            summary: 'API is currently under maintenance.'
+            status: 'Maintenance'
+            active: true
+          }
+        ] : (sample.title === 'Helpdesk' ) ? [
+          {
+            submitted: moment().subtract(5, 'minutes').calendar(),
+            content: '',
+            summary: 'Helpdesk is experiencing issues.',
+            status: 'Down',
+            active: true
+          }
+        ] : []
       }
     })
   },
   saveEndpoints (endpoints: Array<Endpoint>): void {
     localStorage.setItem('endpoints', JSON.stringify(endpoints))
   },
-  getMessages (): Array<Message> {
-    let messages = JSON.parse(localStorage.getItem('messages') || '[]')
-    return (messages.length > 0) ? messages : [
-      { id: 1, text: 'API is currently under maintenance.', status: 'Maintenance' },
-      { id: 2, text: 'Helpdesk is experiencing issues.', status: 'Down' }
-    ]
-  },
-  saveMessages (messages: Array<Message>): void {
-    return JSON.parse(localStorage.getItem('messages'))
+  reset (): void {
+    localStorage.removeItem('endpoints')
   }
 }
