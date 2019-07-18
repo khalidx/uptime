988fe77a5906ad7fa386dcf86a2468390bf221b6
Khalid Zoabi <kzoabi@outlook.com>
2019-07-18 10:09:01 -0400

    Adding the `/services/remove` implementation

--- src/components/RemoveService.vue
+++ src/components/RemoveService.vue
@@ -1,26 +1,53 @@
 <template>
-  <a class="btn btn-outline-secondary btn-hero-sm btn-hero-success" href="#" @click="comingSoon">
-    <i class="fa fa-rss"></i> <span class="d-none d-sm-inline-block ml-1">{{ text }}</span>
-  </a>
+  <div>
+    <p><em>Remove a service</em><p>
+    <p><strong>Select the service to remove</strong>
+    <ul class="list-group push mt-3">
+      <li
+        v-for="endpoint in endpoints" :key="endpoint.title"
+        class="list-group-item d-flex justify-content-between align-items-center"
+        @click="remove(endpoint)"
+        >
+
+        <router-link
+          :to="`/status/${endpoint.name}`"
+          tag="a">
+          {{ endpoint.title }}
+        </router-link>
+
+        <div v-html="status(endpoint)"></div>
+
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
+import data from '../services/data'
+
 export default Vue.extend({
   data () {
     return {
-      clicked: false
+      endpoints: data.getEndpoints()
     }
   },
   methods: {
-    comingSoon () {
-      this.clicked = !this.clicked
-    }
-  },
-  computed: {
-    text () {
-      return this.clicked ? 'Coming soon!' : 'Subscribe'
+    remove (endpoint) {
+      this.endpoints = this.endpoints.filter(e => e.name !== endpoint.name)
+      data.saveEndpoints(this.endpoints)
+    },
+    status (endpoint) {
+      let recentMessage = endpoint.messages.slice().reverse().find(message => message.active)
+      let status = (recentMessage) ? recentMessage.status: endpoint.status
+      let className = status === 'Operational' ? 'badge-success'
+        : status === 'Maintenance' ? 'badge-warning'
+          : status === 'Down' ? 'badge-danger'
+            : undefined
+      return (className)
+        ? `<span class="badge badge-pill ${className}">${status}</span>`
+        : `<span class="badge badge-pill">${status}</span>`
     }
   }
 })
