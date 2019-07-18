88d1d78bc9543248399f4e9c803530017fd8d7c9
Khalid Zoabi <kzoabi@outlook.com>
2019-07-18 10:07:46 -0400

    Adding download functionality to the JSON export component.

--- src/components/Export.vue
+++ src/components/Export.vue
@@ -2,16 +2,6 @@
   <div>
     <a
       class="btn btn-primary btn-hero-sm btn-hero-success pull-right m-3"
-      :href="`data:text/json;charset=utf-8,${encodeURIComponent(messages)}`"
-      download="messages.json">
-      <i class="fa fa-arrow-down"></i> <span class="d-none d-sm-inline-block ml-1">Download</span>
-    </a>
-    <div class="shadow-lg p-3 mb-3 bg-white rounded">
-      <h3>Messages</h3>
-      <pre>{{ messages }}</pre>
-    </div>
-    <a
-      class="btn btn-primary btn-hero-sm btn-hero-success pull-right m-3"
       :href="`data:text/json;charset=utf-8,${encodeURIComponent(endpoints)}`"
       download="endpoints.json">
       <i class="fa fa-arrow-down"></i> <span class="d-none d-sm-inline-block ml-1">Download</span>
@@ -31,7 +21,6 @@ import data from '../services/data'
 export default Vue.extend({
   data () {
     return {
-      messages: JSON.stringify(data.getMessages(), null, 2),
       endpoints: JSON.stringify(data.getEndpoints(), null, 2)
     }
   }
