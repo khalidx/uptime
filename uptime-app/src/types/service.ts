088d737a01520b5bd7e7ae1b8570cadc19cf9f9d
Khalid Zoabi <kzoabi@outlook.com>
2019-08-02 01:40:00 -0400

    Updating the `Service` schema with validation rules

--- uptime-app/src/types/service.ts
+++ uptime-app/src/types/service.ts
@@ -12,33 +12,34 @@ export default interface Service {
 export const schema = Joi.object().keys({
   id: Joi.string().uuid().required(),
   name: Joi.string().min(1).max(30).required(),
-  location: Joi.allow(Joi.string().uri(), Joi.string().uri()).required(),
-  status: Joi.allow('Operational', 'Maintenance', 'Down')
-// requests: Array<{
-//   start: string
-//   end: string
-//   latency: string
-//   response: {
-//     type: 'success' | 'error'
-//     message: string
-//     raw: string
-//   }
-// }>
-// checks: Array<{
-//   cron: string
-//   lastCheck: string
-//   nextCheck: string
-// }>
-// feedback: Array<{
-//   submitted: string,
-//   content: string
-// }>
-// messages: Array<{
-//   id: string
-//   submitted: string
-//   content: string
-//   summary: string
-//   status: Status
-//   active: boolean
-// }>
+  title: Joi.string().min(1).max(30).required(),
+  location: Joi.string().uri().required(),
+  status: Joi.allow('Operational', 'Maintenance', 'Down').required(),
+  requests: Joi.array().required().items(Joi.object().keys({
+    start: Joi.string().isoDate().required(),
+    end: Joi.string().isoDate().required(),
+    latency: Joi.number().integer().unit('milliseconds').required(),
+    response: Joi.object().required().keys({
+      type: Joi.allow('success', 'error').required(),
+      message: Joi.string().min(1).max(30).required(),
+      raw: Joi.string().min(1).max(500).required()
+    })
+  })),
+  checks: Joi.array().required().items(Joi.object().keys({
+    cron: Joi.string().min(1).max(30).required(),
+    lastCheck: Joi.string().isoDate().required(),
+    nextCheck: Joi.string().isoDate().required()
+  })),
+  feedback: Joi.array().required().items(Joi.object().keys({
+    submitted: Joi.string().isoDate().required(),
+    content: Joi.string().min(1).max(280).required()
+  })),
+  messages: Joi.array().required().items(Joi.object().keys({
+    id: Joi.string().uuid().required(),
+    submitted: Joi.string().isoDate().required(),
+    summary: Joi.string().min(1).max(280).required(),
+    content: Joi.string().max(3000).optional(),
+    status: Joi.allow('Operational', 'Maintenance', 'Down').required(),
+    active: Joi.bool().required()
+  }))
 })
