22f20163f1e692f899ac09d7c1e5c6cab1cffe63
Khalid Zoabi <kzoabi@outlook.com>
2019-08-01 23:24:39 -0400

    Removing DynamoDb local client initialization from the dynamoDb utility (since we aren't using dynamodb-local with serverless right now).

--- uptime-app/src/utilities/dynamoDb.ts
+++ uptime-app/src/utilities/dynamoDb.ts
@@ -1,10 +1,3 @@
 import AWS from 'aws-sdk'
 
-export default (process.env.IS_OFFLINE)
-  ? new AWS.DynamoDB.DocumentClient({
-    region: 'localhost',
-    endpoint: 'http://localhost:8000',
-    accessKeyId: 'NONE',
-    secretAccessKey: 'NONE'
-  })
-  : new AWS.DynamoDB.DocumentClient()
+export default new AWS.DynamoDB.DocumentClient()
